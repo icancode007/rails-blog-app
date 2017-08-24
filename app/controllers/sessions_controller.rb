4b2d936da09b2deca3541661b8e2e94c9c3b77c5
Jose Franny Rodriguez <icancode911@gmail.com>
2017-08-24 16:12:02 -0400

    fixed up the create action for users so that signup also simulates login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,8 @@ class SessionsController < ApplicationController
       flash[:success] = "You have successfully logged in"
       redirect_to user_path(user)
     else
-
       flash.now[:danger] = "There was something wrong with your login information"
-
       render 'new'
-
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -21,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def update
-
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
